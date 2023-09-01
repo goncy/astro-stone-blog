3ec88637d5617bb9d950e78c9c1b6ed278bb53c7
goncy <gonzalo.pozzo4@gmail.com>
2023-09-01 18:01:50 -0300

    Add changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import react from "@astrojs/react";
+import compress from 'astro-compress';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react(), tailwind(), compress()]
 });

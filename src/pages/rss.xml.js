779a088d207f68478fe36bcee2f0abdc4f2ba838
goncy <gonzalo.pozzo4@gmail.com>
2023-09-02 15:14:41 -0300

    Add prettier

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,15 +2,15 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
 export async function get(context) {
-	const posts = await getCollection('blog');
+  const posts = await getCollection('blog');
 
-	return rss({
-		title: "Gonzalo Pozzo",
-		description: "Solutions Engineer, content creator, frontend developer, in 🖤 with React.",
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/${post.slug}/`,
-		})),
-	});
+  return rss({
+    title: 'Gonzalo Pozzo',
+    description: 'Solutions Engineer, content creator, frontend developer, in 🖤 with React.',
+    site: context.site,
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/${post.slug}/`,
+    })),
+  });
 }
